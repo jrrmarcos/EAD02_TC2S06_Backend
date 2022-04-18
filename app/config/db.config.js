a1770dd8edf4d1db9b9276de3bbe8e362508fff2
Marcos Junior <marcosjunior538@gmail.com>
2022-04-18 13:52:22 -0300

    Variáveis secretas no Glitch

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     //url: "mongodb://localhost:27017/bancoTeste"
-    url: "mongodb+srv://marcos:<password>@cluster0.m3yrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    //url: "mongodb+srv://marcos:<password>@cluster0.m3yrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    
+    //Glitch's secrets variables
+    url: "mongodb+srv://marcos:process.env.[password]@cluster0.m3yrt.mongodb.net/process.env.[db]?retryWrites=true&w=majority"
 };
