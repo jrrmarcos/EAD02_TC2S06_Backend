e6f36a430f7ee874f0eec3f4fb4ee1eeb78aa475
Marcos Junior <marcosjunior538@gmail.com>
2022-04-18 11:53:43 -0300

    Deploy

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/bancoTeste"
+    //url: "mongodb://localhost:27017/bancoTeste"
+    url: "mongodb+srv://marcos:<password>@cluster0.m3yrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 };
